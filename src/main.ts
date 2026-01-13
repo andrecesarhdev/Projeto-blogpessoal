8ed209bfeb4659a0f6ec65fcaf079d80bbba696f
André César <andrecesarhenrique@gmail.com>
2026-01-12 22:41:22 -0300

    cria módulo de postagem e ajusta configuração principal

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// configuraçoes do servidor.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // ajuste para horario do brasil
   process.env.TZ = '-03:00';
 
+  // adiciona validação em toda entrada de dados
   app.useGlobalPipes(new ValidationPipe());
 
+  // limita ou libera acesso aos serviços da minha api/backend
   app.enableCors();
 
+  // abertura de portas para receber dados.
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
